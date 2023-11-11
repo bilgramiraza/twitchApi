a6dd161d0a029f80bb56e356667c63277e944640
bilgramiraza <bilgramiraza@yahoo.com>
2023-11-11 14:29:17 +0530

    Fixed Mongoose Config to remove redundent Options

--- configs/mongoose-config.js
+++ configs/mongoose-config.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const mongodbURI = process.env.MONGODB_URI;
-const mongodbParams = { useNewUrlParser: true, useUnifiedTopology: true };
-mongoose.connect(mongodbURI, mongodbParams);
+mongoose.connect(mongodbURI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB Connection Error:'));
