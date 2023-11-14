de0b7665d45c427c76684b70c97b940ce21f23d6
bilgramiraza <bilgramiraza@yahoo.com>
2023-11-14 18:36:13 +0530

    removed unnecessary nested response

--- controllers/streamInfoController.js
+++ controllers/streamInfoController.js
@@ -4,7 +4,7 @@ const streamInfoRoute= async (req, res)=>{
   const streamerName = req.params.streamerName;
   try{
     const streamData = await getStreamInfo(streamerName);
-    return res.status(200).json({ streamData });
+    return res.status(200).json( streamData );
   }catch(err){
     console.log(err);
     return res.status(500).json({ err });
